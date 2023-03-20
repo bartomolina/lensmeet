8666f8d8c1f5e5e5cfaf3ed1c6adf32588ffe6d3
Barto Molina <bartomolina@gmail.com>
2023-03-20 23:32:35 +0100

    Updated to use apollo client from lens sdk

--- components/user-context.tsx
+++ components/user-context.tsx
@@ -1,12 +1,10 @@
-import { createContext, useContext, useState } from "react";
-import { useWalletLogin, useWalletLogout } from "@lens-protocol/react-web";
+import { createContext, useContext } from "react";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { gql } from "@apollo/client";
-import { getClient, refreshJWT } from "../lib/api";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { gql } from "@apollo/client";
+import { useWalletLogin, useWalletLogout } from "@lens-protocol/react-web";
 
 const UserContext = createContext({
-  accessToken: "",
   signIn: () => {},
   signOut: () => {},
 });
@@ -14,7 +12,6 @@ const UserContext = createContext({
 export const useUser = () => useContext(UserContext);
 
 export const UserProvider = ({ children }: React.PropsWithChildren) => {
-  const [accessToken, setAccessToken] = useState("");
   const { execute: login } = useWalletLogin();
   const { execute: logout } = useWalletLogout();
   const { isConnected } = useAccount();
@@ -33,31 +30,12 @@ export const UserProvider = ({ children }: React.PropsWithChildren) => {
     if (connector instanceof InjectedConnector) {
       const signer = await connector.getSigner();
       await login(signer);
-      const sessionCredentials = window.localStorage.getItem("lens.credentials");
-      if (sessionCredentials) {
-        const credentials = JSON.parse(sessionCredentials);
-
-        const apolloClient = getClient();
-        const result = await apolloClient.mutate({
-          mutation: gql(refreshJWT),
-          variables: {
-            request: {
-              refreshToken: credentials.data.refreshToken,
-            },
-          },
-        });
-        const accessToken = result.data.refresh.accessToken;
-        window.sessionStorage.setItem("lens.accessToken", accessToken);
-        setAccessToken(accessToken);
-      }
     }
   };
 
   const signOut = async () => {
     await logout();
-    window.sessionStorage.removeItem("lens.accessToken");
-    setAccessToken("");
   };
 
-  return <UserContext.Provider value={{ signIn, signOut, accessToken }}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ signIn, signOut }}>{children}</UserContext.Provider>;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,17 @@ import { useEffect, useMemo, useState } from "react";
 import Head from "next/head";
 import useSWR from "swr";
 import axios from "axios";
-import { useActiveProfile, useWalletLogin } from "@lens-protocol/react-web";
+import { useActiveProfile, useApolloClient } from "@lens-protocol/react-web";
 import { gql } from "@apollo/client";
-import { getClient, exploreProfiles } from "../lib/api";
+import { exploreProfiles } from "../lib/api";
 import ProfilesList from "../components/profiles-list";
-import { useUser } from "../components/user-context";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const Home = () => {
   const { data } = useSWR("/lenslists/lists/845068988534030337/members", fetcher);
-  const { data: activeProfile, loading: loadingProfile } = useActiveProfile();
-  const { accessToken } = useUser();
-  const { isPending: loginPending } = useWalletLogin();
+  const { data: activeProfile } = useActiveProfile();
+  const { query } = useApolloClient();
   const [profiles, setProfiles] = useState([]);
 
   const lensListsProfiles = useMemo(() => {
@@ -23,24 +21,23 @@ const Home = () => {
 
   useEffect(() => {
     if (lensListsProfiles) {
-      const apolloClient = getClient();
-      apolloClient
-        .query({
-          query: gql(exploreProfiles),
-          variables: {
-            profiles: lensListsProfiles,
-          },
-        })
-        .then((response) => {
-          let members = response?.data?.profiles?.items;
-          members = [...members].sort((a, b) => b.stats.totalPublications - a.stats.totalPublications);
-          members = members.sort((a, b) => a.isFollowedByMe - b.isFollowedByMe);
-          console.log(members);
+      query({
+        query: gql(exploreProfiles),
+        fetchPolicy: "no-cache",
+        variables: {
+          profiles: lensListsProfiles,
+        },
+      }).then((response) => {
+        console.log(response?.data?.profiles?.items.length);
+        let members = response?.data?.profiles?.items;
+        members = [...members].sort((a, b) => b.stats.totalPublications - a.stats.totalPublications);
+        members = members.sort((a, b) => a.isFollowedByMe - b.isFollowedByMe);
+        console.log(members);
 
-          setProfiles(members ?? []);
-        });
+        setProfiles(members ?? []);
+      });
     }
-  }, [lensListsProfiles, accessToken]);
+  }, [lensListsProfiles, activeProfile]);
 
   return (
     <>
