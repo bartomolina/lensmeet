7a2190ca56832469862579276cf41975fbd6955f
Barto Molina <bartomolina@gmail.com>
2023-03-23 01:33:27 +0100

    Updated env file

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -5,7 +5,7 @@ import { getDefaultClient } from "connectkit";
 const wagmiConfig = createClient(
   getDefaultClient({
     appName: "WAGMI",
-    alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_MUMBAI_API,
+    alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_POLYGON_API,
     chains: [polygon],
   })
 );
